bf87bfb63a5075b925681324468bf0fa2e0196b2
Hudson <baileyhuds@gmail.com>
2026-08-21 18:19:10 -0500

    added CSS for letterBox id. added a Wordle class object. added a wordle variable set to a new Wordle(). added a grid of letterBox divs.

--- WordleWithReact/src/App.jsx
+++ WordleWithReact/src/App.jsx
@@ -1,21 +1,50 @@
 import { useState } from 'react'
 import './App.css'
 
+class Wordle{
+  constructor(){
+    this.guesses = [];
+    this.target = "GAMES";
+  }
 
+  getTarget() {
+    return this.target;
+  }
+  getGuesses() {
+    return this.guesses;
+  }
+  addGuess(guess){
+    this.guesses.push(guess);
+  }
+  changeTarget(newTarget){
+    this.target = newTarget;
+  }
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [wordle = new Wordle(), updateWordle] = useState(0);
+  
 
   return (
     <>
       <section id="center">
-        <button
+        <h1>Wordle</h1>
+        <input placeholder='Guess a word...' onClick={updateWordle}></input>
+        <div className="grid">
+          <div id="letterBox">box</div>
+          <div id="letterBox">box</div>
+          <div id="letterBox">box</div>
+          <div id="letterBox">box</div>
+          <div id="letterBox">box</div>
+        </div>
+        {/* <button
           type="button"
           className="counter"
           onClick={() => setCount((count) => count + 1)}
         >
           Count is {count}
-        </button>
+        </button> */}
       </section>
 
       <section id="spacer"></section>
